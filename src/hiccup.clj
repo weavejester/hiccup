990904023f33ea983313920925d3f5a560bd2c07
weavejester <jreeves@weavejester.com>
2009-11-02 21:26:51 +0000

    Initial commit of functions adapted from compojure.html

---
+++ src/hiccup.clj
@@ -0,0 +1,48 @@
+;; Copyright (c) James Reeves. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution. By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
+
+(ns hiccup
+  "Efficiently generate HTML from a Clojure data structure."
+  (:use clojure.contrib.java-utils))
+
+(defn escape-html
+  "Change special characters into HTML character entities."
+  [string]
+  (.. (as-str string)
+    (replace "&"  "&amp;")
+    (replace "<"  "&lt;")
+    (replace ">"  "&gt;")
+    (replace "\"" "&quot;")))
+
+(defn- format-attr
+  "Turn a key-value pair into a pair of attribute-value strings."
+  [[key value]]
+  (cond
+    (true? value) [(as-str key) (escape-html key)]
+    (not value)   nil
+    :otherwise    [(as-str key) (escape-html value)]))
+
+(defn- make-attrs
+  "Turn a map into a string of HTML attributes, sorted by attribute name."
+  [attrs]
+  (apply str
+    (for [[attr value] (sort (map format-attr attrs))]
+      (if attr
+        (str " " attr "=\"" value "\"")))))
+
+(defn- make-tag
+  "Wrap some content in an HTML tag."
+  [tag attrs content]
+  (str* "<" tag (map-to-attrs attrs) ">"
+          content
+        "</" tag ">"))
+
+(defn- make-closed-tag
+  "Make a closed XML tag with no content."
+  [tag attrs]
+  (str* "<" tag (map-to-attrs attrs) " />"))
