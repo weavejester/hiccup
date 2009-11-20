528f80852cbf75820d4009945c3bde58a6f88912
weavejester <jreeves@weavejester.com>
2009-11-20 23:37:05 +0000

    Removed pre-compilation - only made things slower

--- src/hiccup.clj
+++ src/hiccup.clj
@@ -9,7 +9,9 @@
 (ns hiccup
   "Efficiently generate HTML from a Clojure data structure."
   (:use clojure.contrib.def)
-  (:use clojure.contrib.java-utils))
+  (:use clojure.contrib.java-utils)
+  (:import clojure.lang.IPersistentVector)
+  (:import clojure.lang.ISeq))
 
 (defn escape-html
   "Change special characters into HTML character entities."
@@ -21,168 +23,61 @@
     (replace "\"" "&quot;")))
 
 (defn- format-attr
-  "Turn a key-value pair into a pair of attribute-value strings."
-  [[key value]]
-  (cond
-    (true? value) [(as-str key) (escape-html key)]
-    (not value)   nil
-    :otherwise    [(as-str key) (escape-html value)]))
+  "Turn a name/value pair into an attribute stringp"
+  [name value]
+  (str " " (as-str name) "=\"" (escape-html value) "\""))
 
 (defn- make-attrs
-  "Turn a map into a string of HTML attributes, sorted by attribute name."
+  "Turn a map into a string of sorted HTML attributes."
   [attrs]
-  (remove nil?
-    (for [[attr value] (sort (map format-attr attrs))]
-      (if attr
-        (str " " attr "=\"" value "\"")))))
+  (apply str
+    (sort
+      (for [[attr value] attrs]
+        (cond
+          (true? value) (format-attr attr attr)
+          (not value)   ""
+          :otherwise    (format-attr attr value))))))
 
 (defvar- re-tag
   #"([^\s\.#]+)(?:#([^\s\.#]+))?(?:\.([^\s#]+))?"
   "Regular expression that parses a CSS-style id and class from a tag name.")
+
+(defvar- container-tags
+  #{"a" "b" "body" "dd" "div" "dl" "dt" "em" "fieldset" "form" "h1" "h2" "h3"
+    "h4" "h5" "h6" "head" "html" "i" "label" "li" "ol" "pre" "script" "span"
+    "strong" "style" "textarea" "ul"}
+  "A list of tags that need an explicit ending tag when rendered.")
 
 (defn parse-tag-name
   "Parse the id and classes from a tag name."
   [tag]
   (rest (re-matches re-tag (as-str tag))))
 
-(derive clojure.lang.IPersistentVector ::vector)
-(derive clojure.lang.ISeq ::seq)
-
 (defmulti render-html
-  "Render a Clojure data structure to a seq of HTML at runtime."
+  "Render a Clojure data structure to a string of HTML."
   type)
 
-(defn render-tag
-  "Render a HTML tag."
-  [tag-name content]
-  (let [[tag id class] (parse-tag-name tag-name)
-        attributes     (make-attrs {:id id, :class class})]
-    (concat
-      (list* "<" tag attributes)
-      content
-      (list "</" tag ">"))))
+(defmethod render-html IPersistentVector
+  [[tag content]]
+  (let [[tag id class]  (parse-tag-name tag)
+        tag-attrs       {:id id, :class class}
+        [attrs content] (if (map? (first content))
+                          [(merge tag-attrs (first content)), (rest content)]
+                          [tag-attrs content])]
+    (if (or content (container-tags tag))
+      (str "<" tag (make-attrs attrs) ">"
+           (render-html content)
+           "</" tag ">")
+      (str "<" tag (make-attrs attrs) " />"))))
 
-(defn render-attrs
-  "Render an attribute map."
-  [attrs content]
-  (concat
-    (if (map? attrs)
-      (make-attrs attrs))
-    content))
-
-(defn render-content
-  "Render the contents of a tag."
-  [content]
-  (cons
-    ">"
-    (render-html
-      (if (map? (first content))
-        (rest content)
-        content))))
-
-(defmethod render-html ::vector
-  [[tag & content]]
-  (render-tag tag
-    (render-attrs (first content)
-      (render-content content))))
-
-(defmethod render-html ::seq
+(defmethod render-html ISeq
   [coll]
-  (mapcat render-html coll))
+  (apply str (map render-html coll)))
 
-(defmethod render-html :default
-  [x]
-  (list (as-str x)))
+(defmethod render-html :default [x]
+  (as-str x))
 
-(defn quoted?
-  "True if the form is a quoted value."
-  [x]
-  (and (list? x)
-       (= 'quote (first x))))
-
-(defn literal?
-  "True if the value is a literal value."
-  [x]
-  (or (and (not (seq? x))
-           (not (symbol? x)))
-      (quoted? x)))
-
-(derive clojure.lang.IPersistentList ::form)
-(derive clojure.lang.Symbol ::form)
-
-(defmulti compile-html
-  "Render a Clojure data structure to a seq of HTML at compile-time when
-  possible, at runtime when not."
-  type)
-
-(defn- collapse-strs
-  "Concatenate adjacent strings in a sequence."
-  [coll]
-  (reduce
-    (fn [xs y]
-      (if (and (string? (first xs)) (string? y))
-        (cons (str y (first xs)) (rest xs))
-        (cons y xs)))
-    '()
-    (reverse coll)))
-
-(defn compile-seq
-  "Compile a sequence of Clojure data structures into HTML"
-  [coll]
-  (let [forms (collapse-strs (mapcat compile-html coll))]
-    (if (every? literal? forms)
-      `(list ~@forms)
-      `(concat ~@(for [form forms]
-                   (if (literal? form)
-                     `(list ~form)
-                     form))))))
-
-(defn compile-tag
-  "Attempt to pre-render a HTML tag."
-  [tag-name content]
-  (if (literal? tag-name)
-    (render-tag tag-name content)
-   `(render-tag ~tag-name ~(compile-seq content))))
-
-(defn compile-attrs
-  "Attempt to pre-render an attribute map."
-  [attrs content]
-  (if (map? attrs)
-    (render-attrs attrs content)
-    (if-not (literal? attrs)
-     `((render-attrs ~attrs ~(compile-seq content)))
-      content)))
-
-(defn compile-content
-  "Attempt to pre-render the contents of a tag."
-  [content]
-  (cons
-    ">"
-    (cond
-      (map? (first content))
-        (mapcat compile-html (rest content))
-      (literal? (first content))
-        (mapcat compile-html content)
-      :else
-        (concat
-          (take 2 content)
-          (mapcat compile-html (drop 2 content))))))
-
-(defmethod compile-html ::vector
-  [[tag & content]]
-  (compile-tag tag
-    (compile-attrs (first content)
-      (compile-content content))))
-
-(defmethod compile-html ::form
-  [form]
-  `((render-html ~form)))
-
-(defmethod compile-html :default
-  [x]
-  (render-html x))
-
-(defmacro html
-  "Efficiently render a Clojure data structure into a seq of HTML."
+(defn html
+  "Render Clojure data structures to HTML."
   [& content]
-  (compile-seq content))
+  (render-html content))
