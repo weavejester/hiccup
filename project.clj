2e7aafe050d271cdeb86cb98502932284a2d7500
weavejester <jreeves@weavejester.com>
2010-01-26 20:33:13 +0000

    Added Leiningen project.clj file

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject hiccup "0.1"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
--- test/main.clj
+++
@@ -1,7 +0,0 @@
-(ns test.main
-  (:use clojure.contrib.test-is)
-  (:require test.hiccup)
-  (:require test.hiccup.form-helpers))
-
-(run-tests 'test.hiccup
-           'test.hiccup.form-helpers)
