e36841824afee8a7ea9cbec3c9cf81450be4f365
weavejester <jreeves@weavejester.com>
2010-03-07 22:15:49 +0000

    Changed Clojure-Contrib version to 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject hiccup "0.2.1"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
+                 [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
