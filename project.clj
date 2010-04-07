874dac676b6ed5354fb5189fc12effd2b0ac2971
weavejester <jreeves@weavejester.com>
2010-04-07 01:10:38 +0100

    Updated project version to 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hiccup "0.2.2"
+(defproject hiccup "0.2.3"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
