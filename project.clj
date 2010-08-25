17ff593c05169f2659ec852e86ac09c8f4804d57
weavejester <jreeves@weavejester.com>
2010-08-25 20:43:07 +0100

    Updated dependencies to Clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject hiccup "0.2.6"
+(defproject hiccup "0.3.0"
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
-                     [swank-clojure "1.2.0"]])
+                     [swank-clojure "1.2.1"]])
