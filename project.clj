410ce94a5fda10cbffc460a31ab3f04623f0f026
weavejester <jreeves@weavejester.com>
2010-09-18 17:21:41 +0100

    Released 0.2.7

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject hiccup "0.2.6"
+(defproject hiccup "0.2.7"
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
-                     [swank-clojure "1.2.0"]])
+  :dev-dependencies [[lein-clojars "0.6.0"]
+                     [swank-clojure "1.2.1"]])
