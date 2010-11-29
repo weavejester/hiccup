04273a4a25321f5f5c1fb85d4c8aab412b73fa4a
weavejester <jreeves@weavejester.com>
2010-11-29 20:53:52 +0000

    Excluded Clojure dependencies from autodoc

--- project.clj
+++ project.clj
@@ -1,16 +1,20 @@
 (defproject hiccup "0.3.1"
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[autodoc "0.7.1"]]
-  :autodoc {:name "Hiccup"
-            :description "A fast library for rendering HTML in Clojure."
-            :copyright "Copyright 2009-2010 James Reeves"
-            :root "."
-            :source-path "src"
-            :web-src-dir "http://github.com/weavejester/hiccup/blob/"
-            :web-home "http://weavejester.github.com/hiccup"
-            :output-path "autodoc"
-            :namespaces-to-document ["hiccup"]
-            :load-except-list [#"/test/" #"project.clj"]})
+  :dependencies
+    [[org.clojure/clojure "1.2.0"]
+     [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies 
+    [[autodoc "0.7.1" :exclusions [org.clojure/clojure
+                                   org.clojure/clojure-contrib]]]
+  :autodoc 
+    {:name "Hiccup"
+     :description "A fast library for rendering HTML in Clojure."
+     :copyright "Copyright 2009-2010 James Reeves"
+     :root "."
+     :source-path "src"
+     :web-src-dir "http://github.com/weavejester/hiccup/blob/"
+     :web-home "http://weavejester.github.com/hiccup"
+     :output-path "autodoc"
+     :namespaces-to-document ["hiccup"]
+     :load-except-list [#"/test/" #"project.clj"]})
