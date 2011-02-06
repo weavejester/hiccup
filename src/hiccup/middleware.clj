0ea367c1d51bf50cf62be1883108de6b3a102cb9
James Reeves <jreeves@weavejester.com>
2011-02-06 16:30:54 +0000

    Middleware wrap-base-url now supports request context

--- src/hiccup/middleware.clj
+++ src/hiccup/middleware.clj
@@ -3,8 +3,10 @@
   (:use hiccup.core))
 
 (defn wrap-base-url
-  "Ring middleware that wraps the handler in the with-base-url function."
-  [handler url]
+  "Ring middleware that wraps the handler in the with-base-url function. The
+  base URL may be specified as an argument. Otherwise, the :context key on the
+  request map is used."
+  [handler & [base-url]]
   (fn [request]
-    (with-base-url url
+    (with-base-url (or base-url (:context request))
       (handler request))))
