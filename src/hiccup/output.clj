ae7c6821cc5d3742ab1a9f5b694f83e644afc099
James Reeves <jreeves@weavejester.com>
2011-08-21 20:00:45 +0100

    Replaced hiccup.core with stub that uses new compiler

--- src/hiccup/output.clj
+++ src/hiccup/output.clj
@@ -2,16 +2,26 @@
   "Renders nested maps representing a HTML DOM into a string."
   (:use hiccup.util))
 
-(defn to-string [node]
-  (with-out-str
-    (print (str "<" (as-str (:tag node))))
-    (when-not (empty? (:attrs node))
-      (print " ")
-      (doseq [[k v] (:attrs node)]
-        (print (str (as-str k) "=\"" (escape-html v) "\""))))
-    (print ">")
-    (doseq [content (:content node)]
-      (if (map? content)
-        (render content)
-        (print (escape-html content))))
-    (print (str "</" (as-str (:tag node)) ">"))))
+(def ^{:doc "A list of tags that need an explicit ending tag when rendered."
+       :private true}
+  container-tags
+  #{"a" "b" "body" "canvas" "dd" "div" "dl" "dt" "em" "fieldset" "form" "h1" "h2"
+    "h3" "h4" "h5" "h6" "head" "html" "i" "iframe" "label" "li" "ol" "option"
+    "pre" "script" "span" "strong" "style" "table" "textarea" "ul"})
+
+(defn to-string
+  ([node]
+     (with-out-str
+       (print (str "<" (as-str (:tag node))))
+       (when-not (empty? (:attrs node))
+         (print " ")
+         (doseq [[k v] (:attrs node)]
+           (print (str (as-str k) "=\"" (escape-html v) "\""))))
+       (print ">")
+       (doseq [content (:content node)]
+         (if (map? content)
+           (to-string content)
+           (print (escape-html content))))
+       (print (str "</" (as-str (:tag node)) ">"))))
+  ([n & nodes]
+     (apply str (map to-string (cons n nodes)))))
