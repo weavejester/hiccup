297e07cfb766935b0c7334a1c8f4f42b0a803819
James Reeves <jreeves@weavejester.com>
2011-12-25 20:52:18 +0000

    Updated Clojure version to 1.2.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject hiccup "0.3.7"
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"]])
