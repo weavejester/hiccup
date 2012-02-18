1df909601922d3f598956993fbc6ace08472eba8
James Reeves <jreeves@weavejester.com>
2012-02-18 17:14:17 +0000

    Better docstring for with-base-url

--- src/hiccup/util.clj
+++ src/hiccup/util.clj
@@ -7,7 +7,8 @@
 (def ^:dynamic *base-url* nil)
 
 (defmacro with-base-url
-  "Sets a base URL that will be prepended onto relative URIs."
+  "Sets a base URL that will be prepended onto relative URIs. Note that for this
+  to work correctly, it needs to be placed outside the html macro."
   [base-url & body]
   `(binding [*base-url* ~base-url]
      ~@body))
