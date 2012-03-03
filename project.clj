148b42190c2905cb993fd85157839dd639614340
James Reeves <jreeves@weavejester.com>
2012-03-03 15:14:18 +0000

    Added Codox as a dev dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject hiccup "1.0.0-beta1"
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[codox "0.5.0"]]
+  :codox {:exclude [hiccup.compiler]})
