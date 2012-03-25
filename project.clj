35fb402614b1bf36bcfef6aab653284b5a7e0f00
James Reeves <jreeves@weavejester.com>
2012-03-25 18:51:49 +0100

    Updated project.clj to Lein 2 syntax

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[codox "0.5.0"]]
+  :plugins [[codox "0.6.0"]]
   :codox {:exclude [hiccup.compiler]})
