1645a610224cfe400a4b6dd5de9c804dc9520872
James Reeves <jreeves@weavejester.com>
2012-04-05 17:53:38 +0100

    Added 'attr-map?' to defelem arglists

--- src/hiccup/def.clj
+++ src/hiccup/def.clj
@@ -24,10 +24,15 @@
           (apply vector tag (first args) body)))
       (apply func args))))
 
+(defn- update-arglists [arglists]
+  (for [args arglists]
+    (vec (cons 'attr-map? args))))
+
 (defmacro defelem
   "Defines a function that will return a element vector. If the first argument
   passed to the resulting function is a map, it merges it with the attribute
   map of the returned element value."
   [name & fdecl]
   `(do (defn ~name ~@fdecl)
+       (alter-meta! (var ~name) update-in [:arglists] #'update-arglists)
        (alter-var-root (var ~name) wrap-attrs)))
