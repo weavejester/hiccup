9e394cb09e051c08a42a735f6c0fdb20f87bd321
James Reeves <jreeves@weavejester.com>
2012-04-11 18:36:59 +0100

    Updated Codox plugin to 0.6.1

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :plugins [[codox "0.6.0"]]
+  :plugins [[codox "0.6.1"]]
   :codox {:exclude [hiccup.compiler]})
