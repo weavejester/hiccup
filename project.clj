9322dd18142833ef6eefa69e628661ce8a3643f3
James Reeves <jreeves@weavejester.com>
2012-09-03 00:28:27 +0100

    Added profiles for Clojure 1.3 and 1.4

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://github.com/weavejester/hiccup"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :plugins [[codox "0.6.1"]]
-  :codox {:exclude [hiccup.compiler]})
+  :codox {:exclude [hiccup.compiler]}
+  :profiles
+  {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
