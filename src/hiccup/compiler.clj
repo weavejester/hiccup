c7beae30c1dd2270fdb4dfe7dd213daa8452fdb0
Paul Dorman <paul.dorman@gmail.com>
2012-09-17 11:06:47 +1200

    Alternate implementation of normalize-element function which allows for element id replacement and/or concatination of additional classes if included in the optional attr-map argument.

--- src/hiccup/compiler.clj
+++ src/hiccup/compiler.clj
@@ -46,13 +46,20 @@
   [[tag & content]]
   (when (not (or (keyword? tag) (symbol? tag) (string? tag)))
     (throw (IllegalArgumentException. (str tag " is not a valid element name."))))
-  (let [[_ tag id class] (re-matches re-tag (as-str tag))
-        tag-attrs        {:id id
-                          :class (if class (.replace ^String class "." " "))}
-        map-attrs        (first content)]
-    (if (map? map-attrs)
-      [tag (merge tag-attrs map-attrs) (next content)]
-      [tag tag-attrs content])))
+  (let [[_ tag id class] (re-matches re-tag (name tag))
+        id (or (:id (first content) id))
+        class (if class
+                (-> class
+                    (.replace "." " ")
+                    (str " " (:class (first content)))
+                    .trim)
+                (:class (first content)))
+        attrs (-> (first content)
+                  (#(if (map? %) % {}))
+                  (assoc :id id :class class))]
+    (if (map? (first content))
+      [tag attrs (next content)]
+      [tag attrs content])))
 
 (defmulti render-html
   "Turn a Clojure data type into a string of HTML."
