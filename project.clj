519db3ea7c11bb4c14d6dcaf0f8aa66ecc95f14d
James Reeves <jreeves@weavejester.com>
2012-11-17 12:02:21 -0500

    Updated Codox with doc source links

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :plugins [[codox "0.6.1"]]
-  :codox {:exclude [hiccup.compiler]}
+  :plugins [[codox "0.6.3"]]
+  :codox {:exclude [hiccup.compiler]
+          :src-dir-uri "http://github.com/weavejester/hiccup/blob/1.0.2"
+          :src-linenum-anchor-prefix "L"}
   :profiles
   {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
