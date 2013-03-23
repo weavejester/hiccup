bb8a7eeb06555c96f4bbe57b7a1aed6094a85d3f
James Reeves <jreeves@weavejester.com>
2013-03-23 13:47:28 +0000

    Tests with Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
           :src-linenum-anchor-prefix "L"}
   :profiles
   {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
