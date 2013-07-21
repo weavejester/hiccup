weavejester/hiccup

commit 663b6638ea7509cfcff1bb9b5eb3890be14014b7

replace multimethod with protocol

src/hiccup/compiler.clj
@@ -54,10 +54,9 @@
       [tag (merge tag-attrs map-attrs) (next content)]
       [tag tag-attrs content])))
 
-(defmulti render-html
-  "Turn a Clojure data type into a string of HTML."
-  {:private true}
-  type)
+(defprotocol HtmlRenderer
+  (render-html [this]
+    "Turn a Clojure data type into a string of HTML."))
 
 (defn- render-element
   "Render an element vector as a HTML element."
@@ -69,15 +68,19 @@
            "</" tag ">")
       (str "<" tag (render-attr-map attrs) (end-tag)))))
 
-(defmethod render-html IPersistentVector
-  [element]
-  (render-element element))
-
-(defmethod render-html ISeq [coll]
-  (apply str (map render-html coll)))
-
-(defmethod render-html :default [x]
-  (as-str x))
+(extend-protocol HtmlRenderer
+  IPersistentVector
+  (render-html [this]
+    (render-element this))
+  ISeq
+  (render-html [this]
+    (apply str (map render-html this)))
+  Object
+  (render-html [this]
+    (str this))
+  nil
+  (render-html [this]
+    ""))
 
 (defn- unevaluated?
   "True if the expression has not been evaluated."

commit 68e5d9676371ddecedc30c0cba6409ad09bf3ae7

Fixed keyword rendering (fixes #80)
Bug introduced by commit 663b663

src/hiccup/compiler.clj
@@ -1,7 +1,7 @@
 (ns hiccup.compiler
   "Internal functions for compilation."
   (:use hiccup.util)
-  (:import [clojure.lang IPersistentVector ISeq]))
+  (:import [clojure.lang IPersistentVector ISeq Named]))
 
 (def ^:dynamic *html-mode* :xml)
 
@@ -75,6 +75,9 @@
   ISeq
   (render-html [this]
     (apply str (map render-html this)))
+  Named
+  (render-html [this]
+    (name this))
   Object
   (render-html [this]
     (str this))

test/hiccup/test/core.clj
@@ -36,6 +36,8 @@
   (testing "seqs are expanded"
     (is (= (html [:body (list "foo" "bar")]) "<body>foobar</body>"))
     (is (= (html (list [:p "a"] [:p "b"])) "<p>a</p><p>b</p>")))
+  (testing "keywords are turned into strings"
+    (is (= (html [:div :foo]) "<div>foo</div>")))
   (testing "vecs don't expand - error if vec doesn't have tag name"
     (is (thrown? IllegalArgumentException
                  (html (vector [:p "a"] [:p "b"])))))