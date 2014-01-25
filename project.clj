edbfce02110ff2a7fd0b2044222fd638d6367e53
James Reeves <jreeves@weavejester.com>
2014-01-25 17:42:35 +0000

    Released 1.0.5

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject hiccup "1.0.4"
+(defproject hiccup "1.0.5"
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :plugins [[codox "0.6.6"]]
   :codox {:exclude [hiccup.compiler]
-          :src-dir-uri "http://github.com/weavejester/hiccup/blob/1.0.4/"
+          :src-dir-uri "http://github.com/weavejester/hiccup/blob/1.0.5/"
           :src-linenum-anchor-prefix "L"}
   :profiles
   {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
