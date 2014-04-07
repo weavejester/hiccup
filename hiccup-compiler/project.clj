03339e88ecc7e5aa67bb4c6e31110018247d9a46
Janne Asmala <janne@asmala.name>
2014-04-08 06:01:48 +0800

    Cleaned up repo for pull request.

--- hiccup-compiler/project.clj
+++ hiccup-compiler/project.clj
@@ -4,8 +4,9 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [hiccup/hiccup-common "1.0.5"]]
   :aliases {"test-all"
-            ["with-profile" "dev:1.3:1.4:1.5" "test"]}
+            ["with-profile" "dev:1.3:1.4:1.5:1.6" "test"]}
   :profiles
   {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
