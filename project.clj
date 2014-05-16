4476dcc083797fb8c39c6b8de4bd3e461626ced7
John Wiseman <jjwiseman@gmail.com>
2014-05-16 14:39:06 -0700

    Added :license to project.clj.
    This makes it easier to audit licenses with tools like lein-licenses.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject hiccup "1.0.5"
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
+  :license {:name "Eclipse Public License 1.0"}
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :plugins [[codox "0.7.4"]]
   :codox {:exclude [hiccup.compiler]
