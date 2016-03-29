fb7467a2c34b972b59a2ada22e69bb297785b90e
James Reeves <jreeves@weavejester.com>
2016-03-29 17:21:41 +0100

    Update minimum Clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://github.com/weavejester/hiccup"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[codox "0.7.4"]]
   :codox {:exclude [hiccup.compiler]
           :sources ["src"]
           :src-dir-uri "http://github.com/weavejester/hiccup/blob/1.0.5/"
           :src-linenum-anchor-prefix "L"}
-  :aliases {"test-all" ["with-profile" "default:+1.3:+1.4:+1.5" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8" "test"]}
   :profiles
-  {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+  {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
