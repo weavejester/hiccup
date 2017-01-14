a8e676d031f06cfb74dc133b4b358739ccb38daf
James Reeves <jreeves@weavejester.com>
2017-01-14 22:26:20 +0000

    Add docstring to hiccup.page/doctype

--- src/hiccup/page.clj
+++ src/hiccup/page.clj
@@ -5,6 +5,7 @@
             [hiccup.util :as util]))
 
 (def doctype
+  "Map of doctype strings."
   {:html4
    (util/raw-string "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" "
                     "\"http://www.w3.org/TR/html4/strict.dtd\">\n")
