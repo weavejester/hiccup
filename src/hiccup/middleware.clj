e2636c488eb7a71e5ed0630aa62b8d58c9fb8e28
James Reeves <jreeves@weavejester.com>
2017-01-14 14:06:36 +0000

    Avoid use of '& [args]' in wrap-base-url

--- src/hiccup/middleware.clj
+++ src/hiccup/middleware.clj
@@ -6,7 +6,9 @@
   "Ring middleware that wraps the handler in the with-base-url function. The
   base URL may be specified as an argument. Otherwise, the :context key on the
   request map is used."
-  [handler & [base-url]]
-  (fn [request]
-    (util/with-base-url (or base-url (:context request))
-      (handler request))))
+  ([handler]
+   (wrap-base-url handler nil))
+  ([handler base-url]
+   (fn [request]
+     (util/with-base-url (or base-url (:context request))
+       (handler request)))))
