b35896faa26955f357456a733e56b8ec0fd14bf5
James Reeves <jreeves@weavejester.com>
2017-01-14 04:19:33 +0000

    Update Codox to 0.10.2

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-codox "0.9.4"]]
+  :plugins [[lein-codox "0.10.2"]]
   :codox
   {:output-path "codox"
    :source-uri  "http://github.com/weavejester/hiccup/blob/{version}/{filepath}#L{line}"
-   :namespaces  [#"^hiccup\.(?!compiler)"]}
+   :namespaces  [#"^hiccup2?\.(?!compiler)"]}
   :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8" "test"]}
   :profiles
   {:dev {:dependencies [[criterium "0.4.4"]]}
