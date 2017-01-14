d835384bd12c9b16ecf140ba2debca8cd5934e9f
James Reeves <jreeves@weavejester.com>
2017-01-14 04:05:45 +0000

    Improve docs in hiccup.core and hiccup2.core

--- src/hiccup/core.clj
+++ src/hiccup/core.clj
@@ -5,14 +5,21 @@
             [hiccup.util :as util]))
 
 (defmacro html
-  "Render Clojure data structures to a string of HTML."
+  "Render Clojure data structures to a string of HTML. Strings are NOT
+  automatically escaped, but must be manually escaped with the h function.
+
+  A literal option map may be specified as the first argument. It accepts one
+  keys that controls how the HTML is outputted:
+
+    :mode - one of :html, :xhtml, :xml or :sgml (defaults to :xhtml)"
+  {:deprecated "2.0"}
   [options & content]
   (if (map? options)
     `(str (hiccup2/html ~(assoc options :escape-strings? false) ~@content))
     `(str (hiccup2/html {:escape-strings? false} ~options ~@content))))
 
 (defn h
-  "Escape strings when the :escape-strings? option is false."
+  "Escape strings within the html macro."
   [text]
   (if util/*escape-strings?*
     (util/as-str text)
