6e9bcca11a24ef2318c9816f24315a50383aafba
James Reeves <jreeves@weavejester.com>
2023-06-21 21:41:59 +0100

    Update test profiles

--- project.clj
+++ project.clj
@@ -10,10 +10,11 @@
    :source-uri  "http://github.com/weavejester/hiccup/blob/{version}/{filepath}#L{line}"
    :namespaces  [#"^hiccup2?\.(?!compiler)"]
    :metadata    {:doc/format :markdown}}
-  :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "test"]}
   :profiles
   {:dev {:dependencies [[criterium "0.4.4"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+   :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}}
   :global-vars {*warn-on-reflection* true})
