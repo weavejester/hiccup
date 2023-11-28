00ddf9996eca193606763a071334bfa1fcc0fd20
Esko Luontola <esko@luontola.fi>
2023-11-28 20:31:51 +0200

    Optimize to generate less bytecode
    It was possible for the macros to generate so much bytecode that they
    would exceed Java's 64KB limit for method code size. In such situations
    the Clojure compiler would fail with the message "Method code too
    large!" See https://github.com/weavejester/hiccup/issues/205
    
    This commit does the following optimizations:
    
    1. Concatenate string literals at compile time, so that we can replace
       multiple StringBuilder.append() calls with just one. This reduces the
       generated code from O(n) to O(1). This also improves performance by
       10-20%, since copying one long string is faster than many short
       strings.
    
    2. When a runtime check is needed to determine whether a value is the
       element's attribute map or its first child element, avoid duplicating
       the code for the element's content. This reduces the generated code
       from O(n^2) to O(n).
    
    While improving the test coverage, some edge cases of generating bad
    HTML were detected. This commit doesn't change the existing behavior,
    but only documents it in the new tests. Fixing that behavior will be
    done in future commits.

---
+++ test/hiccup2/optimizations_test.clj
@@ -0,0 +1,78 @@
+(ns hiccup2.optimizations-test
+  (:require [clojure.test :refer :all]
+            [clojure.walk :as walk]
+            [hiccup2.core :as h]))
+
+(defn- count-forms [data]
+  (count (filter seq? (tree-seq coll? seq data))))
+
+(deftest method-code-size
+  ;; With Hiccup 2.0.0-RC2, it was easy to cause the hiccup2.core/html macro to
+  ;; generate so much bytecode that it would go over the 64KB limit of how much
+  ;; bytecode one Java method may contain. It would crash the Clojure compiler
+  ;; with a "Method code too large!" exception. These are a regression tests for
+  ;; that. See https://github.com/weavejester/hiccup/issues/205
+
+  (testing "static elements should be concatenated to one string, also when they have dynamic sibling elements"
+    (let [baseline     (walk/macroexpand-all
+                        `(h/html [:div
+                                  [:p]
+                                  (identity nil)
+                                  [:p]]))
+          pathological (walk/macroexpand-all
+                        `(h/html [:div
+                                  [:p] [:p] [:p] [:p] [:p]
+                                  (identity nil)
+                                  [:p] [:p] [:p] [:p] [:p]]))]
+      (is (= (count-forms baseline)
+             (count-forms pathological)))))
+
+  (testing "code size should grow O(n), instead of O(n^2), as more dynamic first-child elements are added"
+    (let [example-0 (walk/macroexpand-all
+                     `(h/html [:div
+                               [:div
+                                [:div
+                                 [:div
+                                  [:div]]]]]))
+          example-1 (walk/macroexpand-all
+                     `(h/html [:div (identity nil)
+                               [:div
+                                [:div
+                                 [:div
+                                  [:div]]]]]))
+          example-2 (walk/macroexpand-all
+                     `(h/html [:div (identity nil)
+                               [:div (identity nil)
+                                [:div
+                                 [:div
+                                  [:div]]]]]))
+          example-3 (walk/macroexpand-all
+                     `(h/html [:div (identity nil)
+                               [:div (identity nil)
+                                [:div (identity nil)
+                                 [:div
+                                  [:div]]]]]))
+          example-4 (walk/macroexpand-all
+                     `(h/html [:div (identity nil)
+                               [:div (identity nil)
+                                [:div (identity nil)
+                                 [:div (identity nil)
+                                  [:div]]]]]))
+          example-5 (walk/macroexpand-all
+                     `(h/html [:div (identity nil)
+                               [:div (identity nil)
+                                [:div (identity nil)
+                                 [:div (identity nil)
+                                  [:div (identity nil)]]]]]))
+          examples  [example-0
+                     example-1
+                     example-2
+                     example-3
+                     example-4
+                     example-5]
+          diffs     (->> examples
+                         (map count-forms)
+                         (partition 2 1)
+                         (map (fn [[a b]] (- b a))))]
+      (is (< (apply max diffs)
+             (* 1.1 (apply min diffs)))))))
